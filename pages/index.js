0501b2a300837965305bf65a96aff0447b6a0697
Ayush Adarsh <ayushadarsh03@gmail.com>
2022-02-19 17:46:27 +0530

    tsx to js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+// import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
@@ -7,7 +7,7 @@ import MediumCard from '../components/MediumCard'
 import LargeCard from '../components/LargeCard'
 import Footer from '../components/Footer'
 
-const Home: NextPage = ({ exploreData, cardsData }) => {
+const Home = ({ exploreData, cardsData }) => {
   return (
     <div className="">
       <Head>
@@ -19,33 +19,30 @@ const Home: NextPage = ({ exploreData, cardsData }) => {
 
       <Header />
       <Banner />
-      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
-        <section className='pt-6'>
-          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+      <main className="mx-auto max-w-7xl px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="pb-5 text-4xl font-semibold">Explore Nearby</h2>
 
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {exploreData.map((item: any) => (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData.map((item) => (
               <SmallCard
                 key={item.img}
-                img={item.img} distance={item.distance} location={item.location}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
               />
             ))}
           </div>
 
-
           {/* Pull data from server */}
         </section>
 
-        <section >
-          <h2 className='text-4xl font-semibold pb-5  mt-3'>
-            Live Anywhere
-          </h2>
+        <section>
+          <h2 className="mt-3 pb-5 text-4xl  font-semibold">Live Anywhere</h2>
 
-          <div className='flex space-x-3 overflow-scroll scroll scrollbar-hide p-3'>
+          <div className="scroll flex space-x-3 overflow-scroll p-3 scrollbar-hide">
             {cardsData?.map(({ img, title }) => (
-              <MediumCard
-                key={img}
-                img={img} title={title} />
+              <MediumCard key={img} img={img} title={title} />
             ))}
           </div>
         </section>
@@ -55,36 +52,33 @@ const Home: NextPage = ({ exploreData, cardsData }) => {
             img="https://links.papareact.com/4cj"
             title="The Greatest Outdoors"
             description="Wishlists curated by Airbnb"
-            buttonText={"Get Inspired"}
+            buttonText={'Get Inspired'}
           />
         </section>
-
       </main>
 
       <Footer />
 
       {/* Footer */}
-    </div >
+    </div>
   )
 }
 
 export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  )
 
-  const exploreData = await fetch('https://links.papareact.com/pyp').
-    then((res) => res.json())
-
-  const cardsData = await fetch('https://links.papareact.com/zp1').
-    then((res) => res.json())
-
-
+  const cardsData = await fetch('https://links.papareact.com/zp1').then((res) =>
+    res.json()
+  )
 
   return {
     props: {
       exploreData: exploreData,
-      cardsData: cardsData
-    }
+      cardsData: cardsData,
+    },
   }
 }
-
 
 export default Home
